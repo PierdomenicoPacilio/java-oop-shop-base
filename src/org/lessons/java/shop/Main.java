9e5dcba7261ec2aef0bbaba424dd8d30a12183ec
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-06-27 18:46:31 +0200

    incapsulamento dei dati sensibili + overload metodi

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -5,11 +5,53 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
         Prodotto iphone16 = new Prodotto("Iphone 16",
-                "Benvenuto nel futuro. Con iPhone 16, l'esperienza d'uso si evolve, diventando straordinariamente intuitiva e potente. Progettato da zero per integrare Apple Intelligence, questo non è solo uno smartphone: è il tuo assistente personale, il tuo studio creativo e la tua finestra sul mondo, tutto in un design elegante e resistente.",
+                "Apple",
                 new BigDecimal(797),
                 new BigDecimal(0.22));
+
+        System.out.println(iphone16.GetNome());
+        System.out.println(iphone16.GetMarca());
+        System.out.println(iphone16.GetCodice());
         System.out.println(iphone16.GetNomeEsteso());
         System.out.println("$ " + iphone16.GetPrezzoBase());
+        System.out.println(iphone16.GetIva() + "%");
         System.out.println("$ " + iphone16.GetPrezzoIva());
+
+        System.out.println("");
+
+        // ESEMPIO DI INSERIMENTO SBAGLIATO
+        iphone16.SetNome("");
+        iphone16.SetMarca("");
+        iphone16.SetPrezzoBase(new BigDecimal(-20));
+        iphone16.SetIva(new BigDecimal(-5));
+
+        System.out.println("");
+
+        System.out.println(iphone16.GetNome());
+        System.out.println(iphone16.GetMarca());
+        System.out.println(iphone16.GetCodice());
+        System.out.println(iphone16.GetNomeEsteso());
+        System.out.println("$ " + iphone16.GetPrezzoBase());
+        System.out.println(iphone16.GetIva() + "%");
+        System.out.println("$ " + iphone16.GetPrezzoIva());
+
+        System.out.println("");
+
+        // ESEMPIO DI INSERIMENTO GIUSTO
+        iphone16.SetNome("Samsung Galaxy");
+        iphone16.SetMarca("Samsung");
+        iphone16.SetPrezzoBase(new BigDecimal(795));
+        iphone16.SetIva(new BigDecimal(0.15));
+
+        System.out.println("");
+
+        System.out.println(iphone16.GetNome());
+        System.out.println(iphone16.GetMarca());
+        System.out.println(iphone16.GetCodice());
+        System.out.println(iphone16.GetNomeEsteso());
+        System.out.println("$ " + iphone16.GetPrezzoBase());
+        System.out.println(iphone16.GetIva() + "%");
+        System.out.println("$ " + iphone16.GetPrezzoIva());
+
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -6,24 +6,90 @@ import java.util.Random;
 
 public class Prodotto {
 
-    int codice;
-    String nome;
-    String descrizione;
-    BigDecimal prezzo;
-    BigDecimal iva;
+    private int codice;
+    private String nome;
+    private String marca;
+    private BigDecimal prezzo;
+    private BigDecimal iva;
 
     Random generatore = new Random();
 
-    public Prodotto(String nome, String descrizione, BigDecimal prezzo, BigDecimal iva) {
+    // COSTRUTTORI
+    public Prodotto(String nome, String marca, BigDecimal prezzo, BigDecimal iva) {
         this.codice = generatore.nextInt(100000, 999999);
         this.nome = nome;
-        this.descrizione = descrizione;
+        this.marca = marca;
         this.prezzo = prezzo;
         this.iva = iva;
     }
 
+    public Prodotto() {
+        this.codice = generatore.nextInt(100000, 999999);
+        this.nome = "Prodotto senza nome";
+        this.marca = "Marca del prodotto";
+        this.prezzo = new BigDecimal(0);
+        this.iva = new BigDecimal(0);
+    }
+
+    // CODICE
+    public int GetCodice() {
+        return this.codice;
+    }
+
+    // NOME
+    public String GetNome() {
+        return this.nome;
+    }
+
+    public void SetNome(String nome) {
+        if (nome != null && nome.length() > 0) {
+            this.nome = nome;
+        }
+    }
+
+    // NOME ESTESO
+    public String GetNomeEsteso() {
+        if (nome != null && nome.length() > 0) {
+            return codice + " - " + nome;
+        }
+        return null;
+    }
+
+    // MARCA
+    public String GetMarca() {
+        return this.marca;
+    }
+
+    public void SetMarca(String marca) {
+        if (marca != null && marca.length() > 0) {
+            this.marca = marca;
+        }
+    }
+
+    // PREZZO BASE
     public BigDecimal GetPrezzoBase() {
         return this.prezzo.setScale(2);
+    }
+
+    public void SetPrezzoBase(BigDecimal prezzo) {
+        if (prezzo.compareTo(BigDecimal.ZERO) > 0 && prezzo != null) {
+            this.prezzo = prezzo;
+        } else {
+            System.out.println("Prezzo inserito non valido!");
+        }
+    }
+
+    // IVA
+    public BigDecimal GetIva() {
+        return this.iva.multiply(BigDecimal.TEN.multiply(BigDecimal.TEN)).setScale(2, RoundingMode.DOWN);
+    }
+
+    public void SetIva(BigDecimal iva) {
+        if (iva.compareTo(BigDecimal.ZERO) > 0 && iva != null) {
+            this.iva = iva;
+        } else {
+            System.out.println("Iva inserita non valida!");
+        }
     }
 
     public BigDecimal GetPrezzoIva() {
@@ -33,10 +99,4 @@ public class Prodotto {
         return null;
     }
 
-    public String GetNomeEsteso() {
-        if (nome != null) {
-            return codice + " - " + nome;
-        }
-        return null;
-    }
 }
